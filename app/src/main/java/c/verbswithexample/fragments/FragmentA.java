cf13eefc87b8563e3c6ee0bdc69f693d616e619b
a4anddev <a4anddev@gmail.com>
2018-07-09 11:08:59 +0530

    frament Add with Button

--- app/src/main/java/c/verbswithexample/fragments/FragmentA.java
+++ app/src/main/java/c/verbswithexample/fragments/FragmentA.java
@@ -2,17 +2,16 @@ package c.verbswithexample.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MyFragment extends Fragment {
+public class FragmentA extends Fragment{
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_a, container, false);
         return view;
     }
--- app/src/main/java/c/verbswithexample/fragments/MainActivity.java
+++ app/src/main/java/c/verbswithexample/fragments/MainActivity.java
@@ -1,25 +1,49 @@
 package c.verbswithexample.fragments;
 
+
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private FragmentManager manager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         manager = getFragmentManager();
 
-        MyFragment myFragment = new MyFragment();
 
-        FragmentManager manager = getFragmentManager();
+    }
+
+    public void addFragA(View view) {
+        FragmentA fragmentA = new FragmentA();
 
         FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.frameid, myFragment, "myFrag");
+        transaction.add(R.id.container, fragmentA, "fragA");
         transaction.commit();
+
+    }
+
+    public void removeFragA(View view) {
+
+
+    }
+
+    public void addFragB(View view) {
+
+        FragmentB fragmentB = new FragmentB();
+
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.container, fragmentB, "fragA");
+        transaction.commit();
+    }
+
+    public void removeFragB(View view) {
+
 
     }
 }
