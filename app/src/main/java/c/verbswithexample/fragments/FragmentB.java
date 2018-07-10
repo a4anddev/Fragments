537cf3359072b32e4ab6b79950f051f5375e9991
a4anddev <a4anddev@gmail.com>
2018-07-10 08:52:25 +0530

    fragmentA input data send second fragemtnB first we receive data mainactivity after that we send another fragment

--- app/src/main/java/c/verbswithexample/fragments/FragmentB.java
+++ app/src/main/java/c/verbswithexample/fragments/FragmentB.java
@@ -6,13 +6,27 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class FragmentB extends Fragment{
 
+    TextView tresult;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_b, container, false);
+
+        tresult = view.findViewById(R.id.tresult);
+
         return view;
+    }
+
+
+    public void addTwoNumInFragB(int x, int y){
+
+        int result = x + y;
+        tresult.setText("Result : " + result);
+
+
     }
 }
--- app/src/main/java/c/verbswithexample/fragments/MainActivity.java
+++ app/src/main/java/c/verbswithexample/fragments/MainActivity.java
@@ -5,39 +5,61 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity implements MyListner{
 
     private FragmentManager manager;
-    TextView tresult;
+
+    private int firstNum, secondNum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
          manager = getFragmentManager();
-         tresult = findViewById(R.id.tresult);
 
          addFragmentA();
+        addFragmentB();
     }
 
     private void addFragmentA() {
 
         FragmentA fragmentA = new FragmentA();
         FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.container, fragmentA, "fragA");
+        transaction.add(R.id.fragContainerA, fragmentA, "fragA");
         transaction.commit();
-
-
 
     }
 
+    private void addFragmentB() {
+
+        FragmentB fragmentB = new FragmentB();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.fragContainerB, fragmentB, "fragB");
+        transaction.commit();
+
+    }
 
     @Override
     public void addTwoNum(int num1, int num2) {
 
-        int result = num1 + num2;
-        tresult.setText("Result : " + result);
+
+
+        this.firstNum = num1;
+        this.secondNum = num2;
+
+        Toast.makeText(this, "Numbers received in activity : " +num1 + " " + num2 , Toast.LENGTH_SHORT).show();
+    }
+
+    public void sendDataFragmentB(View view) {
+
+    FragmentB fragmentB = (FragmentB) manager.findFragmentByTag("fragB");
+
+    fragmentB.addTwoNumInFragB(firstNum, secondNum);
+
+
     }
 }
